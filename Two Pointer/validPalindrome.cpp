e073154f5b1f56856889205ac69b90da50588c56
turjo4620 <sarkerturjo2022@gmail.com>
2026-06-03 11:33:26 +0600

    revised 3 problems (two pointer)

---
+++ Two Pointer/validPalindrome.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        s.erase(remove_if(s.begin(), s.end(),[](char c){return !(isalnum(c));}), s.end());
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        int i = 0;
+        int j = s.length() - 1;
+        while(i < j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
